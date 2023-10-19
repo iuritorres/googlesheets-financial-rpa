229280992de157c5c36b7f766fcdddbe70c81029
iuritorres <iuri.t1000@gmail.com>
2023-10-18 22:49:33 -0300

    try: testing enviroment data

--- src/main.py
+++ src/main.py
@@ -4,14 +4,18 @@ import pandas as pd
 from GoogleSheet import GoogleSheet
 from GSheetsPermissionLevel import GSheetsPermissionLevel
 
+import os.path
 
-sheet = GoogleSheet(
-    spreadsheet_id = '16tfSBgIJusMr69Zo9SYsNBRNSJzdRb1qFeEuCdmwa1w',
-    permission_level = GSheetsPermissionLevel.WRITE
-)
+# sheet = GoogleSheet(
+#     spreadsheet_id = '16tfSBgIJusMr69Zo9SYsNBRNSJzdRb1qFeEuCdmwa1w',
+#     permission_level = GSheetsPermissionLevel.WRITE
+# )
 
-sheet_df = pd.DataFrame.from_records(
-    sheet.read('A:D'),
-    exclude = ['majorDimension', 'range']
-)
-print(sheet_df)
+# sheet_df = pd.DataFrame.from_records(
+#     sheet.read('A:D'),
+#     exclude = ['majorDimension', 'range']
+# )
+# print(sheet_df)
+
+from dotenv import load_dotenv; load_dotenv()
+print(os.environ['TEST'])
