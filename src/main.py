8c8e371c9479a05082897b4c6441f48e0170ad72
iuritorres <iuri.t1000@gmail.com>
2023-11-09 21:32:54 -0300

    testing env variables gh-actions

--- src/main.py
+++ src/main.py
@@ -6,15 +6,15 @@ from GoogleSheet import GoogleSheet
 from GSheetsPermissionLevel import GSheetsPermissionLevel
 
 # -- Google Sheets
-# sheet = GoogleSheet(
-#     spreadsheet_id = '16tfSBgIJusMr69Zo9SYsNBRNSJzdRb1qFeEuCdmwa1w',
-#     permission_level = GSheetsPermissionLevel.WRITE
-# )
+sheet = GoogleSheet(
+    spreadsheet_id = '16tfSBgIJusMr69Zo9SYsNBRNSJzdRb1qFeEuCdmwa1w',
+    permission_level = GSheetsPermissionLevel.WRITE
+)
 
-# sheet_df = pd.DataFrame.from_records(
-#     sheet.read('A:D'),
-#     exclude = ['majorDimension', 'range']
-# )
+sheet_df = pd.DataFrame.from_records(
+    sheet.read('A:D'),
+    exclude = ['majorDimension', 'range']
+)
 
 
 # Web Scraping
